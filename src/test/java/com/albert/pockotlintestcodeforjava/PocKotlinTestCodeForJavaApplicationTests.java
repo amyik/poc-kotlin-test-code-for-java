e9f2bef21e2a636ba78472e743d71279778537de
albert <amyik100@gmail.com>
2022-12-02 17:05:07 +0900

    created a spring-boot project

---
+++ src/test/java/com/albert/pockotlintestcodeforjava/PocKotlinTestCodeForJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.pockotlintestcodeforjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocKotlinTestCodeForJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
